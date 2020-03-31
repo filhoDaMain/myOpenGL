de991213f5c586d4c1d0a5e4fe7ad47a10843855
filho_da_main <filho_da_main>
2020-03-31 16:57:52 +0100

    First commit

---
+++ build/Debug/GNU-MacOSX/src/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-MacOSX/src/main.o: src/main.cpp lib/include/GLFW/glfw3.h
+
+lib/include/GLFW/glfw3.h:
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=myopengl
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/myopengl
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=myopengl.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/myopengl.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=myopengl
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/myopengl
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=myopengl.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/myopengl.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
