ef3e8c1688bd544dd18dcc201d84e745e96c5d06
filho_da_main <filho_da_main>
2020-04-01 23:27:45 +0100

    using modern OpenGL glBindBuffer()

--- src/main.cpp
+++ src/main.cpp
@@ -34,18 +34,48 @@ int main(void)
     /* Print OpenGL version */
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     
+    /* Moder OpenGL */
+    /* --------------------------------------------- */
+    
+    /* Define my vertices */
+    float positions[6] = {
+        -0.5f, -0.5f,
+        -0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    
+    /* Reserve  OpenGL Buffers */
+    GLuint buffer;      /* buffer ID */
+    glGenBuffers(1 /* how many buffers */, &buffer /* output ID */);
+    
+    /* Bind (select) a buffer to write */
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER,
+                 sizeof(positions)  /* size (Bytes) of new data */,
+                 positions          /* pointer to data being written into buffer */,
+                 GL_STATIC_DRAW);
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         
+        glDrawArrays(GL_TRIANGLES   /* type of primitive to render */,
+                     0              /* starting index of enabled array (bounded buffer) */,
+                     3              /* nr of indices on the array (nr of vertexes) */);
+        
+        #if 0
+        /* Legacy OpenGL */
+        /* --------------------------------------------- */
         /* Triangle */
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);   // vertex 0
         glVertex2f(-0.0f,  0.5f);   // vertex 1
         glVertex2f( 0.5f, -0.5f);   // vertex 2
         glEnd();
+        /* --------------------------------------------- */
+         #endif
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
